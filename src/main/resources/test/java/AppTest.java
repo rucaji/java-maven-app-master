1da90415d25d68c0913267e43d0ba38762c40306
= <=>
2024-01-18 13:22:01 +0100

    commit

---
+++ src/main/resources/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        Application myApp = new Application();
+
+        String result = myApp.getStatus();
+
+        assertEquals("OK", result);
+    }
+
+}
